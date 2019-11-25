7f07e4fffb3e3f0ca019c63966c909f1f48ce2c7
xRamaR <luk.jarnut@gmail.com>
2019-11-25 19:00:20 +0100

    ver 0.1

---
+++ basics.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt-get install vim -y
+sudo apt update && sudo apt upgrade -y
+sudo apt-get dist-upgrade
+sudo apt-get install vim -y
+sudo apt-get install snapd -y
+sudo apt-get install git -y
+sudo apt-get install make -y
+sudo apt-get install gcc -y
+sudo snap install mailspring
+sudo snap install spotify
+sudo apt-get install gimp -y
+sudo apt-get install glipper -y
+sudo apt install gnome-tweaks -y
---
+++ wififix.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd
+cd Downloads
+git clone https://github.com/lwfinger/rtlwifi_new.git
+cd rtlwifi_new/
+
+make
+sudo make install
+sudo modprobe -v rtl8723be ant_sel=2
+sudo ip link set wlo1 up
+echo "options rtl8723be ant_sel=2" | sudo tee /etc/modprobe.d/50-rtl8723be.conf
