a8b2b5d64f8e051f1f69081206db8efca68fb40f
xRamaR <luk.jarnut@gmail.com>
2019-11-26 12:53:42 +0100

    add Chrome

--- basics.sh
+++ basics.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-sudo apt-get install vim -y
 sudo apt update && sudo apt upgrade -y
+sudo apt-get install vim -y
 sudo apt-get dist-upgrade
 sudo apt-get install vim -y
 sudo apt-get install snapd -y
-sudo apt-get install git -y
 sudo apt-get install make -y
 sudo apt-get install gcc -y
 sudo snap install mailspring
@@ -14,3 +13,5 @@ sudo apt-get install gimp -y
 sudo apt install software-properties-common -y
 sudo add-apt-repository ppa:philip.scott/elementary-tweaks -y
 sudo apt install elementary-tweaks -y 
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
